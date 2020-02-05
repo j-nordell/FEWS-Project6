1c7168ac1bbcbeaa2703003dace7b0ef36d854b6
Jennifer <jennifer.nordell@outlook.com>
2020-02-04 19:40:05 -0600

    lots of changes... lots of animations

--- js/app.js
+++ js/app.js
@@ -8,18 +8,25 @@ const keyboard = document.getElementById("qwerty");
 const phraseDisplay = document.getElementById("phrase").getElementsByTagName("ul")[0];
 const startButton = document.getElementsByClassName("btn__reset")[0];
 const overlay = document.getElementById("overlay");
+const answerDiv = document.createElement("div");
+const correctAnswer = document.createElement("h3");
+const title = document.getElementsByClassName("title")[0];
 
 let missed = 0;
 let usedPhrases = [];
 let lastPhrase = '';
+let heartList = document.getElementById("scoreboard").getElementsByTagName("ol")[0];
 
+answerDiv.appendChild(correctAnswer);
+title.parentNode.insertBefore(answerDiv, title.nextSibling);
 
 /*================================
         Event Listeners
 ================================*/
 
 startButton.addEventListener("click", () => {
-  overlay.style.display = "none";
+  showGameBoard();
+  startGame();
 });
 
 keyboard.addEventListener("click", (e) => {
@@ -27,7 +34,17 @@ keyboard.addEventListener("click", (e) => {
     e.target.classList.add("chosen", "flash", "animated");
     e.target.disabled = true;
     let letterFound = checkLetter(e.target);
+    if(!letterFound) {
+      missed++;
+      removeLife();
+    }
+    checkWin();
   }
+});
+
+// Event listener to prevent cheating by selecting and highlighting
+document.addEventListener("mousedown", (e) => {
+  e.preventDefault();
 });
 
 /*================================
@@ -55,13 +72,14 @@ function getPhrase() {
     usedPhrases = [];
   } else {
     // Otherwise, get a random phrase that is not equal to the last Phrase and remove it from phrases and put it in usedPhrases
-    let randomIndex = Math.floor(Math.random() * phrases.length);
+    let randomIndex = getRandIndex(phrases);
     while(phrases[randomIndex] == lastPhrase) {
-      randomIndex = Math.floor(Math.random() * phrases.length);
+      randomIndex = getRandIndex(phrases);
     }
 
     const randomPhrase = phrases.splice(randomIndex, 1)[0];
     usedPhrases.push(randomPhrase);
+    lastPhrase = randomPhrase;
     return randomPhrase;
   }
 }
@@ -91,17 +109,135 @@ function addPhraseToDisplay(currentPhrase) {
 function checkLetter(button) {
   let letters = document.getElementsByClassName("letter");
   let userLetter = button.innerText;
+  let wasFound = null;
   for(let letter of letters) {
     if(userLetter == letter.innerText) {
       letter.classList.add("show", "bounce", "animated");
+      wasFound = userLetter;
     }
+  }
+  return wasFound;
+}
+
+/**
+ * Function to switch a "live" heart to a "dead" heart
+ */
+function removeLife() {
+  let hearts = document.getElementsByTagName("img");
+  hearts[5 - missed].classList.add("animated", "jackInTheBox");
+  hearts[5 -missed].src = "images/lostHeart.png";
+}
+
+/**
+ * Function to determine if a win or lose scenario has occurred and update the overlay if one has been met
+ */
+function checkWin() {
+  let totalLetters = document.getElementsByClassName("letter");
+  let shownLetters = document.getElementsByClassName("show");
+
+  if(totalLetters.length == shownLetters.length){
+    gameOver("win");
+  } else if (missed == 5) {
+    gameOver("lose");
   }
 }
 
+/**
+ * Function to drop the overlay in place and apply messages, fonts, and styling
+ * based on whether the user won or lost
+ * @param {string} winOrLose will be either "win" or "lose"
+ */
 
+function gameOver(winOrLose) {
+  clearHearts();
+  overlay.className = '';
+  overlay.style.display = '';
+  correctAnswer.innerText = `Correct answer: ${lastPhrase}`;
+  resetPhrase();
+  resetKeyboard();
+  let h2 = document.getElementsByTagName("h2")[0];
+  h2.style.textTransform = "capitalize";
+  let message = '';
+  if(winOrLose === "win") {
+    let emoji = winEmojis[getRandIndex(winEmojis)];
+    message = `${winningMessages[getRandIndex(winningMessages)]}!
+               ${String.fromCodePoint(parseInt(emoji, 16))}`;
+    h2.style.fontFamily = "Pacifico";
+    // overlay.classList.add("animated", "slideInLeft");
+  } else {
+    let emoji = loseEmojis[getRandIndex(loseEmojis)];
+    message = `${losingMessages[getRandIndex(losingMessages)]}!
+               ${String.fromCodePoint(parseInt(emoji, 16))}`;
+    h2.style.fontFamily = 'Shadows Into Light';
+    // overlay.classList.add("animated", "slideInRight");
+  }
+  h2.innerText = message;
+  overlay.animation = '';
+  startGradientAnimation();
+}
+
+/**
+ * Function to begin the gradient animation on the overlay
+ */
+function startGradientAnimation(delay="0s") {
+  overlay.style.animation = "gradient 10s ease";
+  overlay.style.animationIterationCount = "infinite";
+  overlay.style.animationDelay = delay;
+}
+
+/**
+ * Function to return a valid integers that corresponds to the array given
+ * @param {array} the array to work against
+ * @returns {Number} 
+ */
+function getRandIndex(array) {
+  return Math.floor(Math.random() * array.length);
+}
+
+/**
+ * Function to reset display and add animation to overlay
+ */
+function showGameBoard() {
+  overlay.style.animation = "";
+  overlay.classList.add("animated", "slideOutUp");
+  overlay.style.display = "";
+}
+
+function resetKeyboard() {
+  let keyboardKeys = keyboard.getElementsByTagName("button");
+  for(let key of keyboardKeys) {
+    key.disabled = false;
+    key.className = '';
+  }
+}
+
+function clearHearts() { 
+  heartList.innerHTML = '';
+}
+
+function addHearts() {
+  let htmlString = '';
+  for(let i = 0; i < 5; i++) {
+    htmlString += `<li class="tries"><img src="images/liveHeart.png" height="35px" width="30px"></li>`;
+  }
+  heartList.innerHTML = htmlString;
+}
+
+function resetPhrase() {
+  phraseDisplay.innerHTML = '';
+}
+
+/**
+ * Function to kick off the main logic loop
+ */
+function startGame() {
+  missed = 0;
+  addHearts();
+  let phrase = getRandomPhraseAsArray(phrases);
+  addPhraseToDisplay(phrase);  
+}
 /*================================
         Main Logic
 ================================*/
-
-let phrase = getRandomPhraseAsArray(phrases);
-addPhraseToDisplay(phrase);
+startGradientAnimation();
+clearHearts();
